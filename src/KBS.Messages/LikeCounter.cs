5f0ce6adcb65b774d12f72bd9aaa3a2b545b5cc0
BramblePie <43555369+BramblePie@users.noreply.github.com>
2018-12-03 10:31:50 +0100

    Clarification of temporality of the like counter

--- src/KBS.Messages/LikeCounter.cs
+++ src/KBS.Messages/LikeCounter.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace KBS.Messages
 {
+    /// <summary>
+    /// Very temporary example of a simple like counter
+    /// </summary>
     public static class ILikeCounter
     {
         private static volatile int likes;
