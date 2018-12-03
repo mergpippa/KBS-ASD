20314325a0a02a53e8281c2b8c5890bb1d6a0ead
Gerwin <gerwinvanbeek97@hotmail.com>
2018-12-03 11:52:50 +0100

    renamed and moved files, also added some xml documentation

---
+++ src/KBS.FauxApplication/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace KBS.FauxApplication
+{
+    public interface Configuration
+    {
+        int Frequency { get; set; }
+         int Size { get; set; }
+        int TestDuration { get; set; }
+        int BatchSize { get; set; }
+        string Protocol { get; set; }
+    }
+}
