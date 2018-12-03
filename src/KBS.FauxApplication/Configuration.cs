d2468b0cf15c0217f098f398da55e13e628a4cce
Leroy Korterink <mergpippa@users.noreply.github.com>
2018-12-03 12:06:34 +0100

    Update src/KBS.FauxApplication/Configuration.cs
    Co-Authored-By: GerwinvBeek <gerwinvanbeek97@hotmail.com>

--- src/KBS.FauxApplication/Configuration.cs
+++ src/KBS.FauxApplication/Configuration.cs
@@ -5,7 +5,7 @@ namespace KBS.FauxApplication
     public interface Configuration
     {
         int Frequency { get; set; }
-         int Size { get; set; }
+        int Size { get; set; }
         int TestDuration { get; set; }
         int BatchSize { get; set; }
         string Protocol { get; set; }
