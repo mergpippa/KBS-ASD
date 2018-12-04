6e9c4777da7d72959d5a41986022a7dedcbfeefe
Gerwin <gerwinvanbeek97@hotmail.com>
2018-12-04 13:24:18 +0100

    Error build fix (missing reference + wrong implementation)

--- src/KBS.Infrastructure/Manager.cs
+++ src/KBS.Infrastructure/Manager.cs
@@ -1,25 +1,25 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using KBS.FauxApplication;
 
 namespace KBS.Infrastructure
 {
     public class Manager : IManager
     {
-        public List<TestEnviroment> GetTests()
-        {
-            return new List<TestEnviroment>();
-        }
-        
-        public TestEnviroment GetTest(int identifier)
-        {
-            return new TestEnviroment();
-         }
-
-        public void CreateTest(ITestConfiguration configuration)
+        public Task CreateTest(ITestConfiguration configuration)
         {
             throw new NotImplementedException();
         }
-        
+
+        public Task<TestEnviroment> GetTest(int identifier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TestEnviroment>> GetTests()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
