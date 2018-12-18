dbb63aabf95abe88158f5c722a0e029b59a7bc45
Arne-Jan <afokkert5@gmail.com>
2018-12-18 15:46:48 +0100

    changed template.json and parameters for deployment in a resourcegroup, also added creating appserviceplan and app with these templates

--- src/KBS.Infrastructure/Resources/AppServiceApp.cs
+++ src/KBS.Infrastructure/Resources/AppServiceApp.cs
@@ -1,32 +1,49 @@
 using System;
+using System.IO;
 using Microsoft.Azure.Management.ResourceManager;
 using Microsoft.Azure.Management.ResourceManager.Models;
+using Microsoft.Rest.Azure.Authentication;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace KBS.Infrastructure.Resources
 {
     internal class AppServiceApp
     {
+        private readonly string subscriptionId = "60b91b72-e8f0-412f-99af-7349fd751812";
+
+        private readonly string clientId = "15785e10-cd50-4151-89d7-480266cdac81";
+
+        private readonly string clientSecret = "oBexNPcDpVERex0uSFs0YLNMwMn3n3PNvQJTb9sUaAs=";
+
+        private readonly string resourceGroupName = "AJTestCS";
+
+        private readonly string deploymentName = "kbs-asd";
+
+        private readonly string resourceGroupLocation = "West Europe";
+
+        private readonly string pathToTemplateFile = "C:\\Users\\ArneJ\\source\\repos\\TestAzure\\TestAzure\\Template.json";
+
+        private readonly string pathToParameterFile = "C:\\Users\\ArneJ\\source\\repos\\TestAzure\\TestAzure\\Parameters.json";
+
+        private readonly string tenantId = "89b6e630-670d-4944-aecf-0056f43045e9";
+
         /// <summary>
         /// Starts a template deployment.
         /// </summary>
-        /// <param name="resourceManagementClient">
-        /// The resource manager client.
-        /// </param>
-        /// <param name="resourceGroupName">
-        /// The name of the resource group.
-        /// </param>
-        /// <param name="deploymentName">
-        /// The name of the deployment.
-        /// </param>
-        /// <param name="templateFileContents">
-        /// The template file contents.
-        /// </param>
-        /// <param name="parameterFileContents">
-        /// The parameter file contents.
-        /// </param>
-        public static void DeployTemplate(ResourceManagementClient resourceManagementClient, string resourceGroupName, string deploymentName, JObject templateFileContents, JObject parameterFileContents)
+        public async void DeployTemplate()
         {
+            // Trying to obtain the credentials
+            var serviceCreds = await ApplicationTokenProvider.LoginSilentAsync(tenantId, clientId, clientSecret);
+
+            // Read the template and parameter file contents
+            JObject templateFileContents = GetJsonFileContents(pathToTemplateFile);
+            JObject parameterFileContents = GetJsonFileContents(pathToParameterFile);
+
+            // Create the resource manager client
+            var resourceManagementClient = new ResourceManagementClient(serviceCreds);
+            resourceManagementClient.SubscriptionId = subscriptionId;
+
             Console.WriteLine(string.Format("Starting template deployment '{0}' in resource group '{1}'", deploymentName, resourceGroupName));
             var deployment = new Deployment();
 
@@ -46,6 +63,28 @@ namespace KBS.Infrastructure.Resources
             catch (Exception e)
             {
                 Console.WriteLine("bagger" + e);
+            }
+        }
+
+        /// <summary>
+        /// Reads a JSON file from the specified path
+        /// </summary>
+        /// <param name="pathToJson">
+        /// The full path to the JSON file
+        /// </param>
+        /// <returns>
+        /// The JSON file contents
+        /// </returns>
+        private JObject GetJsonFileContents(string pathToJson)
+        {
+            JObject templatefileContent = new JObject();
+            using (StreamReader file = File.OpenText(pathToJson))
+            {
+                using (JsonTextReader reader = new JsonTextReader(file))
+                {
+                    templatefileContent = (JObject)JToken.ReadFrom(reader);
+                    return templatefileContent;
+                }
             }
         }
     }
