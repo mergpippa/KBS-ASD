a126b90e20a292f8bb176d3ac33a2ff479859f8d
Leroy Korterink <s1105266@student.windesheim.bl>
2018-12-20 00:07:54 +0100

    #57 Fix messageInterval

--- src/KBS.TestCases/TestCases/AbstractTestCase.cs
+++ src/KBS.TestCases/TestCases/AbstractTestCase.cs
@@ -9,36 +9,42 @@ namespace KBS.TestCases.TestCases
     public abstract class AbstractTestCase
     {
         private readonly TestCaseConfiguration _testCaseConfiguration;
-        
+
         /// <summary>
-        /// 
         /// </summary>
-        /// <param name="testCaseConfiguration"></param>
+        /// <param name="testCaseConfiguration">
+        /// </param>
         protected AbstractTestCase(TestCaseConfiguration testCaseConfiguration)
         {
             _testCaseConfiguration = testCaseConfiguration;
         }
-        
+
         /// <summary>
-        /// Method used to benchmark a service bus. This method calls a function that should send a message
-        /// to the message bus. The frequency and amount of messages that are sent should be configured
-        /// in the TestCaseConfiguration
+        /// Method used to benchmark a service bus. This method calls a function that should send a
+        /// message to the message bus. The frequency and amount of messages that are sent should be
+        /// configured in the TestCaseConfiguration
         /// </summary>
-        /// <param name="callback"></param>
-        /// <returns></returns>
+        /// <param name="callback">
+        /// </param>
+        /// <returns>
+        /// </returns>
         protected async Task Benchmark(Func<int, Task> callback)
         {
+            Console.WriteLine("Starting benchmark");
+
             // Force this method to run asynchronously
             await Task.Yield();
 
-            var messageInterval = (int) (_testCaseConfiguration.MessagesCount / _testCaseConfiguration.Duration.TotalMilliseconds);
-            
+            var messageInterval = (int)(_testCaseConfiguration.Duration.TotalMilliseconds / _testCaseConfiguration.MessagesCount);
+
             for (var index = 0; index < _testCaseConfiguration.MessagesCount; index++)
             {
                 callback(index);
 
                 await Task.Delay(messageInterval);
             }
+
+            Console.WriteLine("Ending benchmark");
         }
     }
 }
