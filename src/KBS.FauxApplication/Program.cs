9430290b6d07067c256c8bb3a9e5896d75428861
Ruben Pannen <ruben@gaaf-online.nl>
2019-01-07 14:27:56 +0100

    Add enviroment writeline to fauxapp main

--- src/KBS.FauxApplication/Program.cs
+++ src/KBS.FauxApplication/Program.cs
@@ -1,9 +1,15 @@
+using System;
+
 namespace KBS.FauxApplication
 {
     internal static class Program
     {
         private static void Main()
         {
+            var environment = Environment.GetEnvironmentVariable("");
+
+            Console.WriteLine(environment);
+
             new Benchmark.Benchmark().Run().Wait();
         }
     }
